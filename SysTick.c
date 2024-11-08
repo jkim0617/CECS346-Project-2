32c636744b2973e71842feefa1ec835bd113d7fe
Bronson Garel <78556724+bronsongarel@users.noreply.github.com>
2024-11-07 20:04:25 -0800

    Update SysTick.c

--- SysTick.c
+++ SysTick.c
@@ -1,7 +1,7 @@
 // SysTick.c
 // Runs on TM4C123
 // Starter file for CECS346 Project 2
-// By Dr. Min He
+// By Jose Ambriz, Bronson Garel, Jonathan Kim, Dylan Ong, Kyle Wyckoff
 
 
 // oscilloscope or LED connected to PF2 for period measurement
@@ -14,16 +14,12 @@
 
 void SysTick_Init(void) {	
 	NVIC_ST_CTRL_R &= ~NVIC_ST_CTRL_ENABLE; // Clear enable bit 
+	NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R&0x1FFFFFFF) | 0x60000000;
 	NVIC_ST_CTRL_R |= NVIC_ST_CTRL_CLK_SRC | NVIC_ST_CTRL_INTEN;
-	NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R&0x00FFFFFF) | 0x00600000;
 }
 
 void SysTick_Start(uint32_t period) {
-	NVIC_ST_RELOAD_R = period * HALF_S - 1;
+	NVIC_ST_RELOAD_R = period - 1;
 	NVIC_ST_CURRENT_R = 0;
 	NVIC_ST_CTRL_R |= NVIC_ST_CTRL_ENABLE;
-}
-
-void SysTick_Stop(void) {
-	NVIC_ST_CTRL_R &= ~NVIC_ST_CTRL_ENABLE; // Clear enable bit 
 }
